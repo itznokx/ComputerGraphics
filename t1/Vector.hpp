53fa9cef040aacbd4e05774315da2eab7bc7a808
itznokx <dimitri.b.medeiros@gmail.com>
2024-10-23 13:32:53 -0300

    t1 folder fix

---
+++ t1/Vector.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <cmath>
+
+class Vec4 {
+	public:
+	int x,y,z,a;
+	Vec4(){
+		std::cout << "Test"
+	}
+};
---
+++ t1/window.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+#include "Vector.hpp"
+
+class WindowSDL {
+	public:	
+	WindowSDL(int width, int height){
+		initializeSDL();
+		this->window = SDL_CreateWindow(
+				"CG WORK",
+				SDL_WINDOWPOS_UNDEFINED,
+				SDL_WINDOWPOS_UNDEFINED,
+				width,
+				height,
+				SDL_WINDOW_SHOWN);
+		if (!window){
+			SDL_Log("Window Creation failed.\nSDL_ERROR: %s", SDL_GetError());
+			SDL_Quit();
+			return;
+		}
+		else{
+			initializeRenderer();
+			if (!renderer){
+				SDL_Log("Renderer Creation failed.\nSDL_ERROR: %s", SDL_GetError());
+				SDL_Quit();
+			}
+		}
+	}
+	void renderScene(Vec4* colorMatrix,int height, int width){
+		bool isRunning = true;
+		SDL_Event event;
+		while (isRunning){
+			while(SDL_PollEvent(&event)){
+				if (event.type == SDL_QUIT){
+					isRunning = false;
+					cleanSDLBuffer();
+				}
+			}
+			for (int lines = 0; lines < height;lines++){
+				for (int collumns = 0; collumns < width;collumns++){
+					SDL_SetRenderDrawColor(this->renderer,colorMatrix->x,colorMatrix->y,colorMatrix->z,colorMatrix->a);
+					SDL_RenderDrawPoint(renderer,lines,collumns);
+				}
+			}
+		}
+	}
+	void cleanSDLBuffer(){
+	 SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+	}
+	private:
+	~WindowSDL(){}
+	int initializeSDL (){
+		if (SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS) <0) {
+			std::cout << "Error SDL Initialization: " << SDL_GetError();
+			return 0;
+		}
+		return 1;
+	}
+	void initializeRenderer() {
+		this->renderer = SDL_CreateRenderer(window,-1,SDL_RENDERER_ACCELERATED);
+
+	}
+	SDL_Window* window;
+	SDL_Renderer* renderer;
+};
