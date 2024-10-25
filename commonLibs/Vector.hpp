a8ad0b0a0cb3b062f72a3dc3d7dd4ecb0f41bb4f
itznokx <dimitri.b.medeiros@gmail.com>
2024-10-25 17:56:22 -0300

    Vec3 and Vec4 classes in commonLib

--- commonLibs/Vector.hpp
+++ commonLibs/Vector.hpp
@@ -1,10 +1,31 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+class Vec3 {
+public:
+	float x,y,z;
+	Vec3(float _x,float _y,float _z) : x(_x),y(_y),z(_z)
+	{}
+	Vec3()
+	{}
+	void setX (float n){x=n;}
+	void setY (float n){y=n;}
+	void setZ (float n){z=n;}
+
+};
 class Vec4 {
 	public:
-	int x,y,z,a;
-	Vec4(){
-		std::cout << "Test"
-	}
+	float x,y,z;
+	int a;
+	Vec4(float _x,float _y,float _z,int _a) : x(_x),y(_y),z(_z),a(_a)
+	{}
+	Vec4(Vec3* v,int _a) : x(v->x),y(v->y),z(v->z),a(_a)
+	{}
+	Vec4()
+	{}
+	void setX (float n){x=n;}
+	void setY (float n){y=n;}
+	void setZ (float n){z=n;}
+	void setA (int n){a=n;}
 };
