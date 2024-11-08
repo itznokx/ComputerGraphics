3a5919babea7d6f81a1ad19e8d4efe293b918029
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-08 20:02:52 -0300

    t2

---
+++ t2/main2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "../commonLibs/window.hpp"
+#include "../commonLibs/scene.hpp"
+
+using namespace std;
+
+int main (void){
+	float rSphere = 5.0f;
+	float dJanela = 5.0f;
+	Sphere s1 = Sphere(Vec3(0,0,(20.0f+rSphere)),rSphere,Vec4(255,0,0,255),1.0f);
+	Sphere s2 = Sphere(Vec3(20,0,(20.0f+rSphere)),rSphere,Vec4(0,255,0,255),1.0f);
+	Sphere s3 = Sphere(Vec3(-20,0,(20.0f+rSphere)),rSphere,Vec4(0,0,255,255),1.0f);
+	std::cout << "Sphere created\n";
+	WindowSDL janela = WindowSDL(500,500);
+	Vec4** matrix = colorMatrix(s1,5.0f,5.0f,
+								500,500,dJanela);
+	std::cout << "Matrix of color created\n";
+	janela.renderScene(matrix);
+	std::cout << "Scene rendered\n";
+	return 0;
+}
