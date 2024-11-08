61cf4e8665f71822bc24d5f2953797fec11537b7
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-08 18:13:52 -0300

    Raycasting

--- commonLibs/raycasting.hpp
+++ commonLibs/raycasting.hpp
@@ -7,16 +7,17 @@ Vec4** colorMatrix (Sphere sphere,float wJanela,float hJanela,int wCanvas,int hC
 	for (int i=0;i<=wCanvas;i++){
 		matrixFinal[i] = new Vec4[wCanvas];
 	}
-
+	Vec3 observer = Vec3(0,0,0);
 	float deltaX = wJanela/wCanvas;
 	float deltaY = hJanela/hCanvas;
 	float zP = -d;
-	for (int lin=0;lin<=hCanvas-1;lin++){
+	for (int lin=0;lin<hCanvas;lin++){
 		float yP = (hJanela/2) - (deltaY/2) - (lin*deltaY);
-		for (int col=0;col<=wCanvas-1;col++){
+		for (int col=0;col<wCanvas;col++){
 			float xP = -(wJanela/2) + (deltaX/2) + (col*deltaX);
-			float deltaInter = sphere.intersects(new Ray(Vec3(0,0,0),Vec3(xP,yP,zP)));
-			if (deltaInter >= 0){
+			Ray* auxRay = new Ray(observer,Vec3(xP,yP,zP)-observer);
+			float deltaInter = sphere.intersects(auxRay);
+			if (deltaInter != -1){
 				matrixFinal[lin][col] = Vec4(255,0,0,255);
 			}
 			else {
