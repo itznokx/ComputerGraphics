ec0f5b469028d6474b5f9a2b13910ce44cfc6b2d
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-08 20:11:27 -0300

    t2 main

--- t2/main2.cpp
+++ t2/main2.cpp
@@ -7,10 +7,14 @@ using namespace std;
 int main (void){
 	float rSphere = 5.0f;
 	float dJanela = 5.0f;
+	Scene cena;
 	Sphere s1 = Sphere(Vec3(0,0,(20.0f+rSphere)),rSphere,Vec4(255,0,0,255),1.0f);
 	Sphere s2 = Sphere(Vec3(20,0,(20.0f+rSphere)),rSphere,Vec4(0,255,0,255),1.0f);
 	Sphere s3 = Sphere(Vec3(-20,0,(20.0f+rSphere)),rSphere,Vec4(0,0,255,255),1.0f);
 	std::cout << "Sphere created\n";
+	cena.insert(&s1);
+	cena.insert(&s2);
+	cena.insert(&s3);
 	WindowSDL janela = WindowSDL(500,500);
 	Vec4** matrix = colorMatrix(s1,5.0f,5.0f,
 								500,500,dJanela);
