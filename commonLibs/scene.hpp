34713272cc8dc75082a56b1ac8def76914a8d951
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-10 22:28:44 -0300

    returnColorScene func, insertLight and insertObjs

--- commonLibs/scene.hpp
+++ commonLibs/scene.hpp
@@ -7,18 +7,54 @@ using namespace std;
 class Scene {
 	vector<Object*> objs;
 	vector<Light*> lights;
+	Light* ambLight;
 	Vec4 bgColor;
 	public:
 		Scene(){
-			sceneObjects = new Object[10];
+			this->objs.reserve(10);
+			this->lights.reserve(1);
 		}
 		Scene(int n){
-			sceneObjects = new Object[n];
+			this->objs.reserve(n);
+			this->lights.reserve(n);
 		}
-		void insert(Object* o){
+		~Scene(){
+			this->objs.clear();
+			this->lights.clear();
+		}
+		void insertObj(Object* o){
+			this->objs.push_back(o);
+		}
+		void setAmbientLight(Vec4 l){
+			this->ambLight = new Light(Vec3(0.0f,0.0f,0.0f),l);
+		}
+		void insertLight(Light* li){
+			this->lights.push_back(li);
+		}
+		Vec4 returnColorScene (Ray* ray){
+			 Object *aux;
+			 float tiMin,tiAux;
+			 bool find = false;
+			for (Object *o : objs ){
+				tiAux = o->intersects(ray);
+				if(tiAux >= 0.0f){
+					find = true;
+					if(tiAux < tiMin){
+						tiMin = tiAux;
+						aux = o;
+					}
+
+				}
+			}
+			if (find == false){
+				return bgColor;
+			}
+			else{
+				return (aux->returnColor(tiMin,ray,lights[0],ambLight));
+			}
 
 		}
+
 	private:
-		Object* sceneObjects;
 
 };
