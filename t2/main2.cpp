e95c729bb2c9003460a7463b63d16b66cd7b79cb
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-12 13:43:32 -0300

    Plane return color

--- t2/main2.cpp
+++ t2/main2.cpp
@@ -8,14 +8,15 @@ int main (void){
 	float rSphere = 40.0f;
 	float dJanela = 30.0f;
 	Scene cena;
-	Sphere s1 = Sphere(Vec3(0,0,-100.0f),rSphere,Vec4(0.7f, 0.0f, 0.0f,1.0f),20.0f);
-	Sphere s2 = Sphere(Vec3(50.0f,0,-100.0f),rSphere,Vec4(0.2f, 0.2f, 0.7f,1.0f),10.0f);
-	Light l1 = Light(Vec3(0.0f,80.0f,-100.0f),Vec4(1.0f, 1.0f, 1.0f,1.0f));
+	Sphere s1 = Sphere(Vec3(0,0,-100.0f),rSphere,Vec4(0.7f, 0.0f, 0.0f,1.0f),10.0f);
+	//Sphere s2 = Sphere(Vec3(50.0f,0,-100.0f),rSphere,Vec4(0.2f, 0.2f, 0.7f,1.0f),10.0f);
+	Plane p1 = Plane(Vec3(0.0f,-rSphere,0.0f),Vec3(0.0f,1.0f,0.0f),Vec4(0.2f,0.7f,0.7f,1.0f),1.0f);
+	Light l1 = Light(Vec3(0.0f,-80.0f,-60.0f),Vec4(1.0f, 1.0f, 1.0f,1.0f));
 	cena.insertLight(&l1);
-	cena.setAmbientLight(Vec4(0.0f, 0.0f, 0.0f,0.0f));
+	cena.setAmbientLight(Vec4(0.3f, 0.3f, 0.3f,1.0f));
 	std::cout << "Sphere created\n";
 	cena.insertObj(&s1);
-	//cena.insertObj(&s2);
+	//cena.insertObj(&p1);
 	//cena.insertObj(&s3);
 	WindowSDL janela = WindowSDL(500,500);
 	Vec4** matrix = colorMatrix(cena,60.0f,60.0f,
