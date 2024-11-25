281791f6b9425f5ba99ab63f14f127744750740d
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-25 12:36:04 -0300

    matrix.hpp

--- commonLibs/matrix.hpp
+++ commonLibs/matrix.hpp
@@ -15,6 +15,41 @@ class Matrix3 {
 public:
 	float x[9];
 	Matrix3(){
-		
+		zeroMatrix();
+	}
+	Matrix3(float x0,float x1,float x2,float x3,float x4,float x5,float x6,float x7,float x8)
+	{
+		x[0] = (x0);
+		x[1] = (x1);
+		x[2] = (x2);
+		x[3] = (x3);
+		x[4] = (x4);
+		x[5] = (x5);
+		x[6] = (x6);
+		x[7] = (x7);
+		x[8] = (x8);
+	}
+	Matrix3(float z[]){0;
+		for (int i = 0; i < sizeof(z);i++){
+			x[i] = z[i];
+		}
+	}
+	void zeroMatrix(){
+		for (int i = 0; i < 9; i++){
+			x[i] = 0;
+		}
+	}
+	void makeIdentity(){
+		for (int i = 0; i < 9; i++){
+			if (i%4==0)
+				x[i] = 1;
+			else
+				x[i] = 0;
+		}
+	}
+	void print(){
+		cout 	<< "| " << x[0] << " , " << x[1] << " , " << x[2] << " |" << "\n"
+				<< "| " << x[3] << " , " << x[4] << " , " << x[5] << " |" << "\n"
+				<< "| " << x[6] << " , " << x[7] << " , " << x[8] << " |" << "\n";
 	}
 } ;
