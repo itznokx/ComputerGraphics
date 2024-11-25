c5dd6f7df3c6f159ab1a1c199f318c66aedab43e
itznokx <dimitri.b.medeiros@gmail.com>
2024-11-25 12:10:41 -0300

    main4.cpp

--- t4/main4.cpp
+++ t4/main4.cpp
@@ -1,6 +1,33 @@
 #include "../commonLibs/window.hpp"
 #include "../commonLibs/scene.hpp"
+#include <cmath>
 int main (void){
-
+	float rSphere = 40.0f;
+	float dJanela = 10.0f;
+	float cilinderX = float(-1/sqrt(3));
+	Scene cena;
+	Light l1 = Light(Vec3(0.0f,20.0f,-30.0f),Vec4(0.7f, 0.7f, 0.7f,1.0f));
+	cena.insertLight(&l1);
+	Sphere s1 = Sphere(Vec3(0.0f,0.0f,-100.0f),rSphere,Vec4(0.7f, 0.2f, 0.2f,1.0f),9.0f);
+	Cilinder c1 = Cilinder(	Vec3(0.0f,0.0f,-100.0f),
+							3*rSphere,rSphere/3,
+							Vec3(cilinderX,cilinderX,cilinderX),
+							Vec4 (0.2f,0.3f,0.8f,1.0f),
+							1.0f);
+	Plane p1 = Plane(Vec3(0.0f,-rSphere,0.0f),
+					 Vec3(0.0f,1.0f,0.0f),
+					 Vec4(0.7f,0.7f,0.2f,1.0f),1.0f);
+	Plane p2 = Plane(Vec3(0.0f,0.0f,-200.0f),
+					 Vec3(0.0f,0.0f,1.0f),
+					 Vec4(0.3f,0.3f,0.7f,1.0f),1.0f);
+	cena.setAmbientLight(Vec4(0.3f, 0.3f, 0.3f,1.0f));
+	cena.insertObj(&s1);
+	//cena.insertObj(&c1);
+	cena.insertObj(&p1);
+	cena.insertObj(&p2);
+	WindowSDL janela = WindowSDL(500,500);
+	uint32_t* matrix = colorMatrix(cena,60.0f,60.0f,
+								500,500,dJanela);
+	janela.renderScene(matrix);
 	return 0;
 }
