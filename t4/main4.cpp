f00b15211ae45faf1b45fcb8ce1738d362b17e86
itznokx <dimitri.b.medeiros@gmail.com>
2025-02-11 11:40:36 -0300

    scene with 1 cilinder and 2 spheres behind, plus 2 planes

--- t4/main4.cpp
+++ t4/main4.cpp
@@ -8,7 +8,8 @@ int main (void){
 	Scene cena;
 	Light l1 = Light(Vec3(0.0f,20.0f,-30.0f),Vec4(0.7f, 0.7f, 0.7f,1.0f));
 	cena.insertLight(&l1);
-	Sphere s1 = Sphere(Vec3(0.0f,0.0f,-100.0f),rSphere,Vec4(0.7f, 0.2f, 0.2f,1.0f),9.0f);
+	Sphere s1 = Sphere(Vec3(-30.0f,0.0f,-100.0f),rSphere,Vec4(0.7f, 0.2f, 0.2f,1.0f),9.0f);
+	Sphere s2 = Sphere(Vec3(30.0f,0.0f,-100.0f),rSphere,Vec4(0.7f, 0.0f, 0.7f,1.0f),9.0f);
 	Cilinder c1 = Cilinder (Vec3(0.0f,0.0f,-50.0f),
 							rSphere*3,
 							rSphere/3,
@@ -16,6 +17,7 @@ int main (void){
 							Vec4 (0.2f,0.3f,0.8f,1.0f),
 							1.0f)
 							;
+
 	Plane p1 = Plane(Vec3(0.0f,-rSphere,0.0f),
 					 Vec3(0.0f,1.0f,0.0f),
 					 Vec4(0.7f,0.7f,0.2f,1.0f),1.0f);
@@ -23,6 +25,8 @@ int main (void){
 					 Vec3(0.0f,0.0f,1.0f),
 					 Vec4(0.3f,0.3f,0.7f,1.0f),1.0f);
 	cena.setAmbientLight(Vec4(0.3f, 0.3f, 0.3f,1.0f));
+	cena.insertObj(&s1);
+	cena.insertObj(&s2);
 	cena.insertObj(&c1);
 	cena.insertObj(&p1);
 	cena.insertObj(&p2);
